90a40a0fb776e5b4ea87059068ccc9d86cd892f6
Keita Tokunaga <ketaketa1985@gmail.com>
2023-01-09 23:54:58 +0900

    binディレクトリ内モジュールのtest実行用に__init__.py追加

--- test/test_app_log.py
+++ test/test_app_log.py
@@ -6,13 +6,13 @@
 #----------------------------------------------
 import pytest 
 
-from ..bin.common.app_log import (
+from bin.common.app_log import (
     entry_log, exec_log
 )
 
-#@entry_log('entry_log')
+@entry_log('entry_log')
 def test_entry_log():
-    assert entry_log('test') == True
+    pass
 
 @exec_log('exec_log')
 def test_entry_log():
