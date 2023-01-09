ca2b486f62a80818a8dd2666b3421efd61e557d9
Keita Tokunaga <ketaketa1985@gmail.com>
2023-01-09 23:25:32 +0900

    vscodeでのpytest実行不可問題についてコメントを追記

--- test/test_app_log.py
+++ test/test_app_log.py
@@ -1,11 +1,18 @@
+#----------------------------------------------
+# * isuue:  
+#   * vscodeでのpytest実行にあたり、以下のエラーで実行できていない。  
+#     DLL load failed while importing _ssl: 指定されたモジュールが見つかりません。
+#       * 現在原因を調査中
+#----------------------------------------------
+import pytest 
 
 from ..bin.common.app_log import (
     entry_log, exec_log
 )
 
+#@entry_log('entry_log')
 def test_entry_log():
-    assert entry_log('test')
-
+    assert entry_log('test') == True
 
 @exec_log('exec_log')
 def test_entry_log():
