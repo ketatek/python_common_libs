4e77329bb4089026cee1891138de19302a810f51
keita.tokunaga <ketaketa1985@gmail.com>
2023-01-12 23:41:38 +0900

    ログ機能のテストコードについて、 catalog fixtureを使用した検証に修正

--- test/test_app_log.py
+++ test/test_app_log.py
@@ -1,13 +1,32 @@
 import pytest 
 
+from logging import DEBUG, ERROR, INFO, WARNING
+
 from bin.common.app_log import (
     entry_log, exec_log
 )
 
-@entry_log('entry_log')
-def test_entry_log():
-    pass
+def test_entry_log(caplog):
+    
+    # ログレベルの設定
+    caplog.set_level(DEBUG)
 
-@exec_log('exec_log')
-def test_exec_log():
-    pass
+    # ログの出力
+    entry_log('entry_log')(lambda x:0)()
+
+    # ログ出力のテスト
+    assert ("entry_log", DEBUG, f'***** entry_log > 開始 *****') in caplog.record_tuples
+    assert ("entry_log", DEBUG, f'***** entry_log > 終了 *****') in caplog.record_tuples
+
+def test_exec_log(caplog):
+
+    # ログレベルの設定
+    caplog.set_level(DEBUG)
+
+    # ログの出力
+    entry_log('exec_log')(lambda x:0)()
+    
+    # ログ出力のテスト
+    assert ("exec_log", DEBUG, f'***** exec_log - 開始 *****') in caplog.record_tuples
+    assert ("exec_log", DEBUG, f'***** exec_log - 終了 *****') in caplog.record_tuples
+    
